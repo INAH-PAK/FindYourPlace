dc6538fa688bc27a3439db32ecdac196b83fa6e8
Ina <inahpakkr@gmail.com>
2022-06-09 11:22:08 +0900

    4일차 메인 레이아웃 짜기

--- app/src/main/java/com/wookie_soft/findyourplace/activities/MainActivity.kt
+++ app/src/main/java/com/wookie_soft/findyourplace/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.wookie_soft.findyourplace.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.wookie_soft.findyourplace.R
 import com.wookie_soft.findyourplace.databinding.ActivityMainBinding
 
@@ -11,6 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        // 옵션 메뉴는 꼭 자바 / 코틀린 코드로 !
+        //1. 툴바를 제목줄로 설정
+        setSupportActionBar(binding.toolbar)
+        // 옵션메뉴 만들기 -> res -> menu 폴더 생성 -> xml 만들기 -> 액티비티가 만들어지기 전에 ㅇ액션바 생성되는 메소드 : onCreateOptionsMenu
+
+
+
+
+
+
 
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
